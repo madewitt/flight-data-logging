4f9d9766292fae5c2f907be55402cd5017f40a16
madewitt <martin.dewitt@gmail.com>
2019-03-16 16:39:02 -0400

    fixed missing terminator

--- collection/FlightSensors.h
+++ collection/FlightSensors.h
@@ -17,7 +17,7 @@ void throttle_isr(void);
 int request_new_spi_measurement(void);
 int get_spi_pressure_data(uint8_t *buffer, size_t bufferLength);
 void spi_pressure_isr(void);
-int request_new_i2c_measurement(void)
+int request_new_i2c_measurement(void);
 void i2c_pressure_isr(void);
 int8_t uart_send(int8_t uartId, uint8_t *uartCmd, int8_t uartCmdLength, uint8_t *uartResponse, int8_t uartResponseLength, int8_t *uartErrorLog, size_t *uartErrorLogCounter);
 void start_imu_fusion_mode(const int UART_IMU_ID, int8_t *uartErrorLog, size_t *uartErrorLogCounter);
